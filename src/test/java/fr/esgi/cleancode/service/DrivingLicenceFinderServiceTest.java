117bb51ec5c54567ff0adb6b3c1aa8169913afe3
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2022-11-30 15:13:41 +0100

    Tests written

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,19 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -18,11 +26,25 @@ class DrivingLicenceFinderServiceTest {
 
     @Test
     void should_find() {
+        UUID id = UUID.randomUUID();
 
+        DrivingLicence drivingLicence = DrivingLicence.builder().id(id).build();
+
+        when(database.findById(id)).thenReturn(Optional.of(drivingLicence));
+
+        val actual = service.findById(id);
+
+        assertThat(actual).contains(drivingLicence);
     }
 
     @Test
     void should_not_find() {
+        UUID id = UUID.randomUUID();
 
+        when(database.findById(id)).thenReturn(Optional.empty());
+
+        val actual = service.findById(id);
+
+        assertThat(actual).isEmpty();
     }
 }
